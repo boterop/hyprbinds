1dda185495621dfccd6d1062613d1e82bfb9428a
boterop <boterop22@gmail.com>
2026-05-16 22:39:56 -0500

    Install without repo

--- install.sh
+++ install.sh
@@ -4,15 +4,29 @@ set -e
 
 mkdir -p ~/.config/hypr/scripts
 
-cp scripts/* ~/.config/hypr/scripts/
+SCRIPT_DIR="scripts"
+CLEAN=false
+
+if ! ls | grep -q "scripts"; then
+  echo "Downloading scripts"
+  git clone https://github.com/boterop/hyprbinds /tmp/hyprbinds
+  SCRIPT_DIR="/tmp/hyprbinds/scripts"
+fi
+
+cp SCRIPT_DIR/* ~/.config/hypr/scripts/
 chmod +x ~/.config/hypr/scripts/hyprbinds.sh
 
 if ! grep -q "hyprbinds.sh" ~/.config/hypr/hyprland.conf; then
-    echo '' >> ~/.config/hypr/hyprland.conf
-    echo '# hyprbinds - script' >> ~/.config/hypr/hyprland.conf
-    echo 'bind = $mainMod, K, exec, ~/.config/hypr/scripts/hyprbinds.sh' >> ~/.config/hypr/hyprland.conf
+  echo '' >>~/.config/hypr/hyprland.conf
+  echo '# hyprbinds - script' >>~/.config/hypr/hyprland.conf
+  echo 'bind = $mainMod, K, exec, ~/.config/hypr/scripts/hyprbinds.sh' >>~/.config/hypr/hyprland.conf
 fi
 
 hyprctl reload
+
+if CLEAN; then
+  echo "Cleaning up"
+  rm -rf /tmp/hyprbinds
+fi
 
 echo "hyprbinds installed successfully"
